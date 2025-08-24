a8b101ac645694bcd3d5c448fdf745a50fbe03a1
aliabbas222004 <aliabbasbhaisaheb22@gmail.com>
2025-08-24 10:53:54 +0530

    INITIAL COMMIT

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ["sihadvancedfrontend.onrender.com"], 
     host: '0.0.0.0', // 👈 important for Render
     port: 5173, // 👈 use Render’s assigned port
   },
